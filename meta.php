21adb89fbc85baee8724f8559310070d406c6e9e
Jake Leahy <jake@leahy.dev>
2023-10-18 11:36:37 +1100

    Add opengraph stuff

--- meta.php
+++ meta.php
@@ -1,4 +1,22 @@
-<meta name="author" content="Jake Leahy" />
-<meta name="generator" content="PHP + Makefile" />
-<meta name="description" content="Portfolio website showing some of the garbage I have gifted to the world" />
-<meta name="keywords" content="potato, Jake Leahy, portfolio" />
+<?php
+
+$PROPERTIES = [
+  // Basic metadata
+  "author" => "Jake Leahy",
+  "generator" => "PHP + Makefile",
+  "description" => "Portfolio website showing some of the garbage I have gifted to the world",
+  "keywords" => "potato, Jake Leahy, portfolio",
+  // Open graph stuff
+  "og:title" => "Jake's site thingy",
+  "og:type" => "website",
+  "og:url" => "https://leahy.dev",
+  "og:image" => "https://leahy.dev/imgs/construction.png",
+  "og:image:width" => "1000",
+  "og:image:height" => "1000",
+  "og:image:alt" => "Bunch of random emojis surronding text saying 'under construction'"
+];
+
+// Render all the metatags
+foreach ($PROPERTIES as $name => $content) {
+  echo "<meta name='$name' content=\"$content\" />";
+}
